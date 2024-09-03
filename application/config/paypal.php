3c0198c6455bc0a3a9151b6a55c6a6f4219a7e98
Zoltan Tajti <31415886+zoltantajti@users.noreply.github.com>
2024-09-03 22:45:07 +0200

    Update paypal.php

--- application/config/paypal.php
+++ application/config/paypal.php
@@ -2,10 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 defined('MODE') OR define('MODE', 'sandbox');
 if(MODE == "sandbox"){
-    $config['client_id'] = 'ASIwIzG4yDVOoATQgV6FFbyefiAEzsk7sj06mo9jbxLguNgqn958ZyeE9NGZ-mOS_TejG2LB3hvtNZUB';
-    $config['client_secret'] = 'ENRCzaqKRPwdIaPkUWhKtR0KClTTULOYJcOyDquOPgZcBU-7Lh_s26EdsdR7DhBYwkMeEtjmJmOGERzR';
+    $config['client_id'] = '***';
+    $config['client_secret'] = '***';
 }elseif(MODE == "live"){
-    $config['client_id'] = 'ASH72Ngtz9FdX_zFhOmZWSYnpCSzb_WgOvISPnCKoWlrV-ojvWxm0B5tBl_Ge1sqqLSPh9RCzABnfnvS';
-    $config['client_secret'] = 'EL67zydAiQWDVDyA1ryOKG1uZA4AlxKojDhvFayGSRdPlNh4c55uzf1dStwR618E18gksLka0HTyusU0';
+    $config['client_id'] = '**';
+    $config['client_secret'] = '**';
 };
 $config['settings'] = array('mode' => MODE,'http.ConnectionTimeOut' => 30,'log.LogEnabled' => true,'log.FileName' => APPPATH . 'logs/paypal.log','log.LogLevel' => 'FINE');
